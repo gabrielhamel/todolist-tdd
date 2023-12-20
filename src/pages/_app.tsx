1353115cfcc146704db8251e5e068576333101f4
Gabriel Hamel <gabriel.hamel.pro@gmail.com>
2023-12-20 18:21:13 +0100

    chore: Install MUI and MUI icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
+import { CssBaseline } from "@mui/material";
 import type { AppProps } from "next/app";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <CssBaseline />
+    <Component {...pageProps} />
+  </>
 );
 
 export default App;
